c2878ea60f6ee508e3e92cf3a21c550c6589bc2f
Csampai Elemér <csampaie.5@gmail.com>
2021-12-14 15:39:34 +0100

    Refactor

---
+++ models/model_mixin.py
@@ -0,0 +1,15 @@
+from db import db
+
+
+class MixinModel():
+  def save_to_db(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete_from_db(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  @classmethod
+  def find_by_attribute(cls, **kwargs):
+    return cls.query.filter_by(**kwargs).first()
---
+++ resources/fleet.py
@@ -0,0 +1,39 @@
+from flask_restful import Resource
+from models.fleet import FleetModel
+from models.model_mixin import MixinModel
+
+
+class Fleet(Resource, MixinModel):
+  def get(self, name):
+    fleet = FleetModel.find_by_attribute(name=name)
+    if fleet:
+      return fleet.json()
+    return {'message': 'Fleet not found'}, 404
+
+  def post(self, name):
+    if FleetModel.find_by_attribute(name=name):
+      return {
+          'message': "A fleet with name '{}' already exists.".format(name)
+      }, 400
+
+    fleet = FleetModel(name)
+    try:
+      fleet.save_to_db()
+    except:
+      return {"message": "An error occurred creating the fleet."}, 500
+
+    return fleet.json(), 201
+
+  def delete(self, name):
+    fleet = FleetModel.find_by_attribute(name=name)
+    if fleet:
+      fleet.delete_from_db()
+
+    return {'message': 'Fleet deleted'}
+
+
+class FleetList(Resource):
+  def get(self):
+    return {
+        'fleets': list(map(lambda car: car.json(), FleetModel.query.all()))
+    }
